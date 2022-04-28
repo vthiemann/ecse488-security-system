1e778d56f7a45536709429c16268fb9de7439389
vthiemann <104582844+vthiemann@users.noreply.github.com>
2022-04-28 07:02:57 -0900

    Add files via upload

---
+++ scripts/main.py
@@ -0,0 +1,17 @@
+import cv2
+import os
+import argparse
+
+from pycoral.adapters.common import input_size
+from pycoral.adapters.detect import get_objects
+from pycoral.utils.dataset import read_label_file
+from pycoral.utils.edgetpu import make_interpreter
+from pycoral.utils.edgetpu import run_inference
+
+def main():
+    #
+
+if __name__ == '__main__':
+    main()
+
+
